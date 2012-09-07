625863c40f7210c5c47bd3e7a19847bc12aeefa7
Wess Cope <wcope@me.com>
2012-09-07 17:40:17 -0400

    initial add

---
+++ app.py
@@ -0,0 +1,42 @@
+from random import randrange
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask("lotripsum")
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+   
+   generated = '....'
+   
+   if request.method == 'POST':
+      if not request.form.get('paragraph_count'):
+         generated = "<h4 style='color:red;text-align:center;>Number of paragraphs are required</h4>"
+      else:
+         
+         paragraph_count = int(request.form['paragraph_count'])
+
+         with open('lotr.txt', 'r') as f:
+            text = f.read()
+            textList = text.split("####-")
+            textlen = len(textList)
+            textlenCap = textlen - paragraph_count
+            starting = randrange(1, textlenCap)
+            ending = starting + paragraph_count
+            paragraphs = "</p><p>".join(textList[starting:ending])
+            generated = u"<p>%s</p>" % paragraphs.decode('utf-8')
+            
+            print
+            print type(textList)
+            print len(textList)
+            print starting
+            print ending
+            print
+            
+   
+   return render_template("index.html", generated=generated)
+   
+   
+app.run(debug=True)
